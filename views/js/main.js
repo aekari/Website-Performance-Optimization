bad90897f68a305b51a32043b23b34aada4f87be
Ashleigh Nichols <ashleighekari@gmail.com>
2017-12-11 10:17:33 -0500

    optimized images

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[0], size);
+  var newwidth = (document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth + dx) + 'px';
+  var randomPizzaContainer =  document.getElementsByClassName('randomPizzaContainer');
+  for (var i = 0; i < randomPizzaContainer.length; i++) {
+    randomPizzaContainer[i].style.width = newwidth;
     }
   }
 
@@ -502,12 +503,13 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-  }
+// scrollTop moved to outside for loop
+var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+for (var i = 0; i < items.length; i++) {
+var phase = Math.sin((scrollTop / 1250) + (i % 5));
+items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+}
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
@@ -526,7 +528,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 30; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
